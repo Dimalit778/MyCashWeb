9819b0a18a80baf9bd19b7e895f4343e1736f615
Dimalit778 <119805398+Dimalit778@users.noreply.github.com>
2024-10-02 17:34:47 +0300

    a

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 
 export const protectRoute = async (req, res, next) => {
+  console.log("req.cookies ->", req.cookies);
   try {
     const token = req.cookies.token;
     if (!token) {

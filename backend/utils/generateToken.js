7e5d17896c65ad2fe1f3b14558061e04813e397a
Dimalit778 <119805398+Dimalit778@users.noreply.github.com>
2024-12-07 21:06:45 +0200

    as

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -13,21 +13,20 @@ export const generateTokenAndSetCookie = (res, userId) => {
     secure: true,
     sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    domain: "localhost",
   });
 
   return token;
 };
 
-export const generateRefreshToken = (res, id) => {
-  const refToken = jwt.sign({ id }, process.env.REFRESH_JWT, {
-    expiresIn: "30d",
-  });
-  res.cookie("refToken", refToken, {
-    path: "/",
-    httpOnly: true,
-    sameSite: "None", // Prevent CSRF attacks
-    secure: true, // Use secure cookies in production
-    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-  });
-};
+// export const generateRefreshToken = (res, id) => {
+//   const refToken = jwt.sign({ id }, process.env.REFRESH_JWT, {
+//     expiresIn: "30d",
+//   });
+//   res.cookie("refToken", refToken, {
+//     path: "/",
+//     httpOnly: true,
+//     sameSite: "None", // Prevent CSRF attacks
+//     secure: true, // Use secure cookies in production
+//     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+//   });
+// };

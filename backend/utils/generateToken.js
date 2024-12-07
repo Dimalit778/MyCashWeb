e420fd615a3783042c16ed2a6eafe38e30c74e10
Dimalit778 <119805398+Dimalit778@users.noreply.github.com>
2024-12-07 20:57:28 +0200

    a

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -13,7 +13,7 @@ export const generateTokenAndSetCookie = (res, userId) => {
     secure: true,
     sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    domain: "http://localhost:3000",
+    domain: "localhost",
   });
 
   return token;

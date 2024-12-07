da94eec3dfc4c9641405f7b76aa6861b48349a0e
Dimalit778 <119805398+Dimalit778@users.noreply.github.com>
2024-12-07 21:19:45 +0200

    s

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 // CORS configuration
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000/"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })

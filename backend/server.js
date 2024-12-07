be3f9aec2f481140ab845757a37ab7691cf78e54
Dimalit778 <119805398+Dimalit778@users.noreply.github.com>
2024-12-07 21:07:06 +0200

    a

--- backend/server.js
+++ backend/server.js
@@ -32,11 +32,8 @@ app.use(
     origin: ["http://localhost:3000"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
-app.set("trust proxy", 1);
 
 // Routes
 app.use("/api/auth", authRoutes);
